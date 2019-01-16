8e3bc7d3d2c5ef1712f4d76d3b423e993696f0e2
Martin Mercer <martinmercer1@gmail.com>
2019-01-15 17:47:16 -0700

    D6/E/Burrito, Complete

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -11,9 +11,28 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    @toppings.push(topping)
+  end
+
+  def remove_topping(topping)
+    @toppings.delete(topping)
+  end
+
+  def change_protein(protein)
+    @protein = protein
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+dinner.add_topping("olives")
+dinner.remove_topping("salsa")
+dinner.change_protein("sausage")
+
+p dinner
