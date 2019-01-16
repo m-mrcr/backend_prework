389d16f3cfea332142ae47eab5d1b6a69fff0185
Martin Mercer <martinmercer1@gmail.com>
2019-01-15 17:54:19 -0700

    D6/E/Person, Complete

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,33 @@
 # person methods below the class so that they print their result to the
 # terminal.
 
-#YOUR CODE HERE
+class Person
+
+  attr_reader :name,
+              :age,
+              :rested
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @rested = true
+  end
+
+  def work
+    @rested = false
+  end
+
+  def sleep
+    @rested = true
+  end
+
+end
+
+frank = Person.new("Frank", 61)
+p frank.name
+p frank.age
+p frank.rested
+frank.work
+p frank.rested
+frank.sleep
+p frank.rested
