b59584ea72c3f286db064c5a396468d395e13407
Martin Mercer <martinmercer1@gmail.com>
2019-01-21 08:30:50 -0700

    Sometimes gems make me feel a false sense of cleverness

---
+++ day_7/projects/10_little_monkeys.rb
@@ -0,0 +1,32 @@
+require 'humanize'
+
+class LittleMonkeys
+
+  def initialize(number)
+    @number = number
+  end
+
+ def rhyme
+   counter = @number
+
+   counter.times do
+     if counter > 1
+       puts "#{counter.humanize.capitalize} little monkeys jumping on the bed,"
+       puts "One fell off and bumped his head,"
+       puts "Mama called the doctor and the doctor said,"
+       puts '"No more monkeys jumping on the bed!"'
+       puts "\n"
+       counter -= 1
+     else
+       puts "One little monkey jumping on the bed,"
+       puts "He fell off and bumped his head,"
+       puts "Mama called the doctor and the doctor said,"
+       puts '"Get those monkeys right to bed!"'
+       counter -= 1
+     end
+   end
+ end
+end
+
+little_monkeys = LittleMonkeys.new(999)
+little_monkeys.rhyme
