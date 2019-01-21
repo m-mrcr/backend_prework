95cbb94556d465c481807342d00a0ae7694a0c39
Martin Mercer <martinmercer1@gmail.com>
2019-01-20 21:51:06 -0700

    changed a couple variable names

--- day_7/projects/ceasar_cipher.rb
+++ day_7/projects/ceasar_cipher.rb
@@ -2,21 +2,21 @@ class CeasarCipher
 
   def encode(message, number)
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-    deconstruct = message.upcase.split(//)
+    deconstruction = message.upcase.split(//)
     rotate_by = alphabet.rotate(number)
     rotated = Hash.new
     rotate_by.each do |letter|
       rotated[letter] = alphabet[rotate_by.index(letter)]
     end
-    encoded_message = []
-    deconstruct.each do |letter|
+    reconstruction = []
+    deconstruction.each do |letter|
       if letter == " "
-        encoded_message << letter
+        reconstruction << letter
       else
-        encoded_message << rotated[letter]
+        reconstruction << rotated[letter]
       end
     end
-    puts encoded_message.join
+    puts reconstruction.join
   end
 end
 
